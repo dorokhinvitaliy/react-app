58061fdbfd20ba9367a4632e30dd1f8b442c43cb
dorokhinvitaliy <dorokhin2006@bk.ru>
2024-10-25 01:49:13 +0300

    v

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -11,6 +12,10 @@ const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
@@ -26,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.className} antialiased`}
       >
         {children}
       </body>
--- src/app/lib/definitions.ts
+++ src/app/lib/definitions.ts
@@ -7,3 +7,9 @@ export type Invoice = {
     // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
     status: 'pending' | 'paid';
   };
+
+  export type Card = {
+    id: string;
+    text: string;
+    color: 'red' | 'blue';
+  }
